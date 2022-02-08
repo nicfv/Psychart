ae9e0cf1a9e29d6a44f7329e4f28df63d4d9798f
ventura <ventura@lbl.gov>
2022-02-07 17:59:34 -0800

    Format incoming data.

--- src/panel.tsx
+++ src/panel.tsx
@@ -8,6 +8,15 @@ interface Props extends PanelProps<PsyOptions> {}
 
 export const PsyPanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
+  const values = data.series
+    .map((serie) => serie.fields)
+    .flat()
+    .map((field) => {
+      return { name: field.name, values: field.values };
+    });
+  console.log(values);
+  console.log(typeof values);
+  console.log(JSON.stringify(values, null, 2));
   State.initPsyChart(width, height, options.unitSystem, options.dbMin, options.dbMax, options.dpMax, theme.isLight);
   return <div dangerouslySetInnerHTML={{ __html: State.getElement().outerHTML }}></div>;
 };
