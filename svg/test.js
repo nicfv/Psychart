c66a80c2b20242f185c81e128dcb40054dcc33ff
ventura <ventura@lbl.gov>
2022-02-10 15:55:10 -0800

    Working on algorithm to process data

---
+++ svg/test.js
@@ -0,0 +1,78 @@
+'use strict';
+
+const data = {
+    series: [
+        {
+            name: 'A',
+            fields: [
+                {
+                    name: 'id',
+                    type: 'number',
+                    values: [1, 2, 3, 4],
+                },
+                {
+                    name: 'timestamp',
+                    type: 'time',
+                    values: [1001, 1002, 1003, 1004],
+                },
+                {
+                    name: 'value',
+                    type: 'number',
+                    values: [10, 20, 30, 40],
+                },
+            ],
+        },
+        {
+            name: 'B',
+            fields: [
+                {
+                    name: 'timestamp',
+                    type: 'time',
+                    values: [1000, 1003, 1004, 1005],
+                },
+                {
+                    name: 'value',
+                    type: 'number',
+                    values: [15, 25, 35, 45],
+                },
+            ],
+        },
+    ],
+};
+
+const timeSeriesData = {
+    '2022-02-10_15_05_00': {
+        'db': 1,
+        'rh': 1,
+    },
+    '2022-02-10_15_05_01': {
+        'db': 1,
+        'rh': 1,
+    },
+    '2022-02-10_15_05_02': {
+        'db': 1,
+        'rh': 1,
+    },
+    '2022-02-10_15_05_03': {
+        'db': 1,
+        'rh': 1,
+    },
+};
+
+window.onload = () => {
+    const pre = document.getElementById('result');
+    const setText = (text) => pre.innerText += JSON.stringify(text, null, 2) + '\n\n';
+
+    setText(data.series.map(serie => {
+        const formatted = {};
+        serie.fields.find(field => field.type === 'time').values.forEach((t, i) => {
+            serie.fields.filter(field => field.type === 'number').forEach(numberField => {
+                console.log(t, i, numberField.name);
+                formatted[t] = formatted[t] || {};
+                formatted[t][numberField.name] = numberField.values[i];
+            })
+        });
+        return formatted;
+    }));
+
+};
