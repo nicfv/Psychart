53abfa103626e2681da3bdc3a2533273e3b120da
ventura <ventura@lbl.gov>
2022-02-10 18:08:24 -0800

    Process data into JSON object, use string-formatted time as keys.

--- src/panel.tsx
+++ src/panel.tsx
@@ -10,19 +10,20 @@ interface Props extends PanelProps<PsyOptions> {}
 export const PsyPanel: React.FC<Props> = ({ options, data, width, height }) => {
   const theme = useTheme();
   data = JSON.parse(JSON.stringify(data));
-  const formatted = new Map();
+  const formatted: { [index: string]: { [index: string]: any } } = {};
   data.series.forEach((serie) => {
     const vals = serie.fields.find((field) => field.type === 'time')?.values;
     if (Array.isArray(vals)) {
       vals?.forEach((t: any, i: any) => {
+        const time = new Date(t).toLocaleString();
         serie.fields
           .filter((field) => field.type === 'number')
           .forEach((numberField) => {
-            if (!formatted.get(t)) {
-              formatted.set(t, new Map());
+            if (!formatted[time]) {
+              formatted[time] = {};
             }
             if (Array.isArray(numberField.values)) {
-              formatted.get(t).set(serie.name + '.' + numberField.name, numberField.values[i]);
+              formatted[time][serie.name + '.' + numberField.name] = numberField.values[i];
             }
           });
       });
