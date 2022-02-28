20f1a1d4a7d8514a47908ee48f516e93a36a1dc9
ventura <ventura@lbl.gov>
2022-02-27 19:14:17 -0800

    Revamped state, now a function, cleaned up panel.tsx

--- src/panel.tsx
+++ src/panel.tsx
@@ -9,11 +9,5 @@ import { format } from 'formatter';
 interface Props extends PanelProps<PsyOptions> {}
 
 export const PsyPanel: React.FC<Props> = ({ options, data, width, height }) => {
-  const isLightTheme = useTheme().isLight,
-    formatted = format(data);
-  console.log(data, formatted);
-  let state = new State();
-  state.initPsyChart(width, height, options, isLightTheme);
-  state.plot(formatted);
-  return <Container child={state.getElement()} />;
+  return <Container child={State(width, height, options, useTheme().isLight, format(data))} />;
 };
--- src/state.ts
+++ src/state.ts
@@ -1,111 +1,74 @@
 import { Psychart } from 'psychart';
 import { PsyOptions } from 'types';
 
-export class State {
-  ps: any;
-  width: number;
-  height: number;
-  options: PsyOptions;
-  isLightTheme: boolean;
-  data: { [index: string]: { [index: string]: number } };
-
-  constructor() {
-    this.ps = null;
-    this.width = 0;
-    this.height = 0;
-    this.options = {
-      unitSystem: 'IP',
-      dbMin: 0,
-      dbMax: 0,
-      dpMax: 0,
-      measurements: 'dbwb',
-      dryBulb: '',
-      wetBulb: '',
-      dewPoint: '',
-      relHum: '',
-      relHumType: 'p',
-    };
-    this.isLightTheme = false;
-    this.data = {};
+export function State(
+  width: number,
+  height: number,
+  options: PsyOptions,
+  isLightTheme: boolean,
+  data: { [index: string]: { [index: string]: number } }
+) {
+  // **** Generate new Psychart **** //
+  const ps = new Psychart(
+    width,
+    height,
+    options.unitSystem === 'IP' ? 1 : 2,
+    options.dbMin,
+    options.dbMax,
+    options.dpMax,
+    isLightTheme ? '#DDD' : '#333',
+    isLightTheme ? '#222' : '#CCD'
+  );
+  // **** Plot data based on measurement type **** //
+  const dbSeries = getInternalSeriesName(options.dryBulb, data);
+  if (!dbSeries) {
+    throw 'No series called ' + options.dryBulb;
   }
-
-  // Compare options `a` and `b` and return true if they are the same.
-  optionsEqual(a: PsyOptions, b: PsyOptions) {
-    return (
-      a.unitSystem === b.unitSystem &&
-      a.dbMin === b.dbMin &&
-      a.dbMax === b.dbMax &&
-      a.dpMax === b.dpMax &&
-      a.measurements === b.measurements &&
-      a.dryBulb === b.dryBulb &&
-      a.wetBulb === b.wetBulb &&
-      a.dewPoint === b.dewPoint &&
-      a.relHum === b.relHum &&
-      a.relHumType === b.relHumType
-    );
-  }
-
-  // Return the full series name that corresponds to the selected field name.
-  getInternalSeriesName = (name: string) =>
-    [
-      ...new Set(
-        Object.keys(this.data)
-          .map((key) => Object.keys(this.data[key]))
-          .flat()
-      ),
-    ].find((x) => x.substring(0, name.length) === name || x.substring(x.length - name.length) === name);
-
-  initPsyChart(width: number, height: number, options: PsyOptions, isLightTheme: boolean) {
-    if (
-      this.width !== width ||
-      this.height !== height ||
-      !this.optionsEqual(this.options, options) ||
-      this.isLightTheme !== isLightTheme
-    ) {
-      this.width = width;
-      this.height = height;
-      this.options = options;
-      this.isLightTheme = isLightTheme;
-      this.ps = new Psychart(
-        this.width,
-        this.height,
-        this.options.unitSystem === 'IP' ? 1 : 2,
-        this.options.dbMin,
-        this.options.dbMax,
-        this.options.dpMax,
-        this.isLightTheme ? '#DDD' : '#333',
-        this.isLightTheme ? '#222' : '#CCD'
-      );
+  switch (options.measurements) {
+    case 'dbwb': {
+      const wbSeries = getInternalSeriesName(options.wetBulb, data);
+      if (!wbSeries) {
+        throw 'No series called ' + options.wetBulb;
+      }
+      for (let t in data) {
+        ps.plotDbWb(data[t][dbSeries], data[t][wbSeries], t, '#03c', 5, 1);
+      }
+      break;
     }
-    // Clear data to trigger a re-plot
-    this.data = {};
-  }
-
-  plot(data: { [index: string]: { [index: string]: number } }) {
-    const oldKeys = Object.keys(this.data),
-      newKeys = Object.keys(data),
-      lastOldKey = oldKeys[oldKeys.length - 1],
-      lastNewKey = newKeys[newKeys.length - 1];
-    if (lastOldKey === lastNewKey) {
-      // Final timestamps are the same.
-      return;
+    case 'dbrh': {
+      const rhSeries = getInternalSeriesName(options.relHum, data),
+        d = options.relHumType === 'p' ? 100 : 1;
+      if (!rhSeries) {
+        throw 'No series called ' + options.relHum;
+      }
+      for (let t in data) {
+        ps.plotDbRh(data[t][dbSeries], data[t][rhSeries] / d, t, '#03c', 5, 1);
+      }
+      break;
     }
-    this.data = data;
-    let dbSeries = this.getInternalSeriesName(this.options.dryBulb),
-      rhSeries = this.getInternalSeriesName(this.options.relHum);
-    if (!dbSeries) {
-      throw 'No series called ' + this.options.dryBulb;
+    case 'dbdp': {
+      const dpSeries = getInternalSeriesName(options.dewPoint, data);
+      if (!dpSeries) {
+        throw 'No series called ' + options.dewPoint;
+      }
+      for (let t in data) {
+        ps.plotDbWb(data[t][dbSeries], data[t][dpSeries], t, '#03c', 5, 1);
+      }
+      break;
     }
-    if (!rhSeries) {
-      throw 'No series called ' + this.options.relHum;
-    }
-    this.ps.clearData();
-    for (let t in data) {
-      this.ps.plotDbRh(data[t][dbSeries], data[t][rhSeries], t, '#03c', 5, 1);
+    default: {
+      throw 'Measurement type ' + options.measurements + ' unsupported.';
     }
   }
-
-  getElement() {
-    return this.ps.el();
-  }
+  return ps.el();
 }
+
+// Return the full series name that corresponds to the selected field name.
+const getInternalSeriesName = (name: string, data: { [index: string]: { [index: string]: number } }) =>
+  [
+    ...new Set(
+      Object.keys(data)
+        .map((key) => Object.keys(data[key]))
+        .flat()
+    ),
+  ].find((x) => x.substring(0, name.length) === name || x.substring(x.length - name.length) === name);
