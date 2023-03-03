c6d9a0676117f7e90d2a7b37570721a21d3c848f
ventura <ventura@lbl.gov>
2023-03-03 11:53:16 -0800

    Added local validation to runner

--- run.sh
+++ run.sh
@@ -2,15 +2,22 @@
 
 for ARG in "${@}" ; do
   if [[ "${ARG}" == -h ]] ; then
-    echo "Usage: ${0} [-h] [-o] [-r] [-P=#.#.#]"
+    echo "Usage: ${0} [-h] [-o] [-r] [-v] [-P=#.#.#]"
     echo '  -h: Show this help message.'
     echo '  -o: Open the svg generator in a web browser.'
     echo '  -r: Starts or restarts Grafana.'
+    echo '  -v: Validate the plugin using grafana/plugin-validator.'
     echo '  -P: Publish the plugin to Grafana. Requires a version number.'
   elif [[ "${ARG}" == -o ]] ; then
     open './docs/index.html'
   elif [[ "${ARG}" == -r ]] ; then
     brew services restart grafana
+  elif [[ "${ARG}" == -v ]] ; then
+    PLUGIN_ID=$(grep '"id"' < src/plugin.json | sed -E 's/.*"id" *: *"(.*)".*/\1/')
+    cp -r dist "${PLUGIN_ID}"
+    zip -qr "${PLUGIN_ID}.zip" "${PLUGIN_ID}"
+    npx @grafana/plugin-validator@latest -sourceCodeUri file://. "${PLUGIN_ID}.zip"
+    rm -r "${PLUGIN_ID}" "${PLUGIN_ID}.zip"
   elif [[ "${ARG}" =~ -P=([0-9]+\.[0-9]+\.[0-9]+) ]] ; then
     VERSION="${BASH_REMATCH[1]}"
     echo "Publishing with version ${VERSION}."
