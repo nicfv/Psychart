1a5fac030b98640fd89bb6a64540f5bd18b621ac
ventura <ventura@lbl.gov>
2023-03-14 12:23:37 -0700

    Add -y option to npx command

--- run.sh
+++ run.sh
@@ -16,7 +16,7 @@ for ARG in "${@}" ; do
     PLUGIN_ID=$(grep '"id"' < src/plugin.json | sed -E 's/.*"id" *: *"(.*)".*/\1/')
     cp -r dist "${PLUGIN_ID}"
     zip -qr "${PLUGIN_ID}.zip" "${PLUGIN_ID}"
-    npx @grafana/plugin-validator@latest -sourceCodeUri file://. "${PLUGIN_ID}.zip"
+    npx -y @grafana/plugin-validator@latest -sourceCodeUri file://. "${PLUGIN_ID}.zip"
     rm -r "${PLUGIN_ID}" "${PLUGIN_ID}.zip"
   elif [[ "${ARG}" =~ -P=([0-9]+\.[0-9]+\.[0-9]+) ]] ; then
     VERSION="${BASH_REMATCH[1]}"
