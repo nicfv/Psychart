a2cc386be0a44b6d1ae0982521dc3a7406f66e13
Nicolas Ventura <ventura@lbl.gov>
2024-03-16 10:13:07 -0700

    Remove load event listener

--- src/app.ts
+++ src/app.ts
@@ -11,125 +11,123 @@ const setVisibility = (id: string, visible: boolean): string => document.getElem
     setOnClick = (id: string, onclick: () => void): void => document.getElementById(id)?.addEventListener('click', onclick),
     isDarkTheme = (): boolean => window.matchMedia("(prefers-color-scheme: dark)").matches;
 
-window.addEventListener('load', () => {
-    setVisibility('svg-container', false);
-    setVisibility('data-input', false);
+setVisibility('svg-container', false);
+setVisibility('data-input', false);
 
-    // Set the window icon
-    document.querySelector('link[rel=icon]')
-        ?.setAttribute('href', require('img/logo.svg'));
+// Set the window icon
+document.querySelector('link[rel=icon]')
+    ?.setAttribute('href', require('img/logo.svg'));
 
-    // Create region checkboxes
-    Psychart.getRegionNamesAndTips().forEach(([name, tip]) => {
-        const checkbox = document.createElement('input'),
-            label = document.createElement('label'),
-            linebreak = document.createElement('br');
-        checkbox.type = 'checkbox';
-        checkbox.id = name;
-        label.setAttribute('for', name);
-        label.textContent = name;
-        label.title = tip;
-        let parent: HTMLElement | null = null;
-        if (name.match(/(Summer|Winter).*/)) {
-            parent = document.getElementById('ashrae-55-container');
-        } else if (name.match(/Data Center.*/)) {
-            parent = document.getElementById('ashrae-dc-container');
-        } else if (name.match(/IBM TS4500.*/)) {
-            parent = document.getElementById('ibm-ts4500-container');
-        }
-        parent?.appendChild(checkbox);
-        parent?.appendChild(label);
-        parent?.appendChild(linebreak);
-    });
-
-    // Add gradient dropdown options
-    Psychart.getGradientNames().forEach(name => {
-        const option = document.createElement('option');
-        option.value = name;
-        option.textContent = name;
-        document.getElementById('gradient')?.appendChild(option);
-    });
-
-    (document.getElementById('gradient') as HTMLSelectElement).addEventListener('change', updateIcon);
-
-    // Define a function to update the icon associated with the gradient.
-    function updateIcon(): void {
-        (document.getElementById('gradicon') as HTMLImageElement)
-            .setAttribute('src', Psychart.getGradientIcon(getStringValue('gradient') as PaletteName));
+// Create region checkboxes
+Psychart.getRegionNamesAndTips().forEach(([name, tip]) => {
+    const checkbox = document.createElement('input'),
+        label = document.createElement('label'),
+        linebreak = document.createElement('br');
+    checkbox.type = 'checkbox';
+    checkbox.id = name;
+    label.setAttribute('for', name);
+    label.textContent = name;
+    label.title = tip;
+    let parent: HTMLElement | null = null;
+    if (name.match(/(Summer|Winter).*/)) {
+        parent = document.getElementById('ashrae-55-container');
+    } else if (name.match(/Data Center.*/)) {
+        parent = document.getElementById('ashrae-dc-container');
+    } else if (name.match(/IBM TS4500.*/)) {
+        parent = document.getElementById('ibm-ts4500-container');
     }
-    updateIcon();
+    parent?.appendChild(checkbox);
+    parent?.appendChild(label);
+    parent?.appendChild(linebreak);
+});
 
-    setOnClick('btnGenerate', () => {
-        const dbMin = getNumericValue('db_min'),
-            dbMax = getNumericValue('db_max'),
-            dpMax = getNumericValue('dp_max');
-        if (dbMin >= dbMax) {
-            alert('Dry bulb min should be less than dry bulb max.');
-        } else if (dpMax > dbMax) {
-            alert('Dew point max should be less than or equal to dry bulb max.');
+// Add gradient dropdown options
+Psychart.getGradientNames().forEach(name => {
+    const option = document.createElement('option');
+    option.value = name;
+    option.textContent = name;
+    document.getElementById('gradient')?.appendChild(option);
+});
+
+(document.getElementById('gradient') as HTMLSelectElement).addEventListener('change', updateIcon);
+
+// Define a function to update the icon associated with the gradient.
+function updateIcon(): void {
+    (document.getElementById('gradicon') as HTMLImageElement)
+        .setAttribute('src', Psychart.getGradientIcon(getStringValue('gradient') as PaletteName));
+}
+updateIcon();
+
+setOnClick('btnGenerate', () => {
+    const dbMin = getNumericValue('db_min'),
+        dbMax = getNumericValue('db_max'),
+        dpMax = getNumericValue('dp_max');
+    if (dbMin >= dbMax) {
+        alert('Dry bulb min should be less than dry bulb max.');
+    } else if (dpMax > dbMax) {
+        alert('Dew point max should be less than or equal to dry bulb max.');
+    } else {
+        ps = new Psychart(
+            {
+                padding: 30,
+                size: { x: 800, y: 600 },
+            } as Layout,
+            {
+                altitude: getNumericValue('alt'),
+                count: 1,
+                dbMax: dbMax,
+                dbMin: dbMin,
+                dpMax: dpMax,
+                regions: Psychart.getRegionNamesAndTips().map(([name,]) => name).filter(name => getCheckedState(name)),
+                series: {
+                    0: {
+                        advanced: getCheckedState('adv'),
+                        dryBulb: '',
+                        gradient: getStringValue('gradient'),
+                        legend: '',
+                        line: getCheckedState('ptLine'),
+                        measurement: 'dbdp',
+                        other: '',
+                        pointRadius: getNumericValue('ptRad'),
+                        relHumType: 'percent',
+                    }
+                },
+                unitSystem: getCheckedState('unitSystem_SI') ? 'SI' : 'IP',
+            } as PsyOptions,
+            Psychart.getDefaultStyleOptions(isDarkTheme()));
+        document.getElementById('svg-container')?.appendChild(ps.getElement());
+        setVisibility('generator', false);
+        setVisibility('svg-container', true);
+        setVisibility('data-input', true);
+    }
+});
+
+setOnClick('btnPlot', () => {
+    const db = getNumericValue('db'),
+        state2 = getNumericValue('state2');
+    if (getCheckedState('measurementType_dbwb')) {
+        if (state2 > db) {
+            alert('Wet bulb is greater than dry bulb temperature!');
         } else {
-            ps = new Psychart(
-                {
-                    padding: 30,
-                    size: { x: 800, y: 600 },
-                } as Layout,
-                {
-                    altitude: getNumericValue('alt'),
-                    count: 1,
-                    dbMax: dbMax,
-                    dbMin: dbMin,
-                    dpMax: dpMax,
-                    regions: Psychart.getRegionNamesAndTips().map(([name,]) => name).filter(name => getCheckedState(name)),
-                    series: {
-                        0: {
-                            advanced: getCheckedState('adv'),
-                            dryBulb: '',
-                            gradient: getStringValue('gradient'),
-                            legend: '',
-                            line: getCheckedState('ptLine'),
-                            measurement: 'dbdp',
-                            other: '',
-                            pointRadius: getNumericValue('ptRad'),
-                            relHumType: 'percent',
-                        }
-                    },
-                    unitSystem: getCheckedState('unitSystem_SI') ? 'SI' : 'IP',
-                } as PsyOptions,
-                Psychart.getDefaultStyleOptions(isDarkTheme()));
-            document.getElementById('svg-container')?.appendChild(ps.getElement());
-            setVisibility('generator', false);
-            setVisibility('svg-container', true);
-            setVisibility('data-input', true);
+            ps.plot({ db: db, other: state2, measurement: 'dbwb' });
         }
-    });
+    } else if (getCheckedState('measurementType_dbdp')) {
+        if (state2 > db) {
+            alert('Dew point is greater than dry bulb temperature!');
+        } else {
+            ps.plot({ db: db, other: state2, measurement: 'dbdp' });
+        }
+    } else if (getCheckedState('measurementType_dbrh')) {
+        if (state2 < 0 || state2 > 100) {
+            alert('Relative humidity is out of bounds! [0%-100%]');
+        } else {
+            ps.plot({ db: db, other: state2, measurement: 'dbrh' });
+        }
+    }
+});
 
-    setOnClick('btnPlot', () => {
-        const db = getNumericValue('db'),
-            state2 = getNumericValue('state2');
-        if (getCheckedState('measurementType_dbwb')) {
-            if (state2 > db) {
-                alert('Wet bulb is greater than dry bulb temperature!');
-            } else {
-                ps.plot({ db: db, other: state2, measurement: 'dbwb' });
-            }
-        } else if (getCheckedState('measurementType_dbdp')) {
-            if (state2 > db) {
-                alert('Dew point is greater than dry bulb temperature!');
-            } else {
-                ps.plot({ db: db, other: state2, measurement: 'dbdp' });
-            }
-        } else if (getCheckedState('measurementType_dbrh')) {
-            if (state2 < 0 || state2 > 100) {
-                alert('Relative humidity is out of bounds! [0%-100%]');
-            } else {
-                ps.plot({ db: db, other: state2, measurement: 'dbrh' });
-            }
-        }
-    });
-
-    setOnClick('btnClear', () => {
-        if (confirm('This will clear ALL data. Are you sure?')) {
-            ps.clearData();
-        }
-    });
+setOnClick('btnClear', () => {
+    if (confirm('This will clear ALL data. Are you sure?')) {
+        ps.clearData();
+    }
 });
