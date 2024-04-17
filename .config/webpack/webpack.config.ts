b7d6d4d6575342b52ae901f2869825bceb8d5766
Nicolas Ventura <ventura@lbl.gov>
2024-04-17 16:05:44 -0700

    Revert the webpack changes

--- .config/webpack/webpack.config.ts
+++ .config/webpack/webpack.config.ts
@@ -149,13 +149,14 @@ const config = async (env): Promise<Configuration> => {
           { from: '../LICENSE', to: '.' },
           { from: '../CHANGELOG.md', to: '.', force: true },
           { from: '**/*.json', to: '.' }, // TODO<Add an error for checking the basic structure of the repo>
-          { from: '**/*.svg', to: '.', noErrorOnMissing: true }, // Optional
+          // { from: '**/*.svg', to: '.', noErrorOnMissing: true }, // Optional
           { from: '**/*.png', to: '.', noErrorOnMissing: true }, // Optional
           { from: '**/*.html', to: '.', noErrorOnMissing: true }, // Optional
-          { from: 'img/**/*', to: '.', noErrorOnMissing: true }, // Optional
+          // { from: 'img/**/*', to: '.', noErrorOnMissing: true }, // Optional
           { from: 'libs/**/*', to: '.', noErrorOnMissing: true }, // Optional
           { from: 'static/**/*', to: '.', noErrorOnMissing: true }, // Optional
           { from: '**/query_help.md', to: '.', noErrorOnMissing: true }, // Optional
+          { from: 'img/logo.svg', to: './img/', noErrorOnMissing: true }, // Copy logo
         ],
       }),
       // Replace certain template-variables in the README and plugin.json
@@ -181,19 +182,19 @@ const config = async (env): Promise<Configuration> => {
       ]),
       ...(env.development
         ? [
-            new LiveReloadPlugin(),
-            new ForkTsCheckerWebpackPlugin({
-              async: Boolean(env.development),
-              issue: {
-                include: [{ file: '**/*.{ts,tsx}' }],
-              },
-              typescript: { configFile: path.join(process.cwd(), 'tsconfig.json') },
-            }),
-            new ESLintPlugin({
-              extensions: ['.ts', '.tsx'],
-              lintDirtyModulesOnly: Boolean(env.development), // don't lint on start, only lint changed files
-            }),
-          ]
+          new LiveReloadPlugin(),
+          new ForkTsCheckerWebpackPlugin({
+            async: Boolean(env.development),
+            issue: {
+              include: [{ file: '**/*.{ts,tsx}' }],
+            },
+            typescript: { configFile: path.join(process.cwd(), 'tsconfig.json') },
+          }),
+          new ESLintPlugin({
+            extensions: ['.ts', '.tsx'],
+            lintDirtyModulesOnly: Boolean(env.development), // don't lint on start, only lint changed files
+          }),
+        ]
         : []),
     ],
 
