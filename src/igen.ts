225e14cfa9663876235f812acf080ca82baacada
Nicolas Ventura <ventura@lbl.gov>
2025-04-01 15:46:47 -0700

    This should work, but document is not defined in nodejs

--- src/igen.ts
+++ src/igen.ts
@@ -1,8 +1,18 @@
 import fs from 'fs';
-import { Psychart } from 'psychart';
+import { GradientNames } from './defaults';
+import { Gradient, Palette } from 'viridis';
 
-for (const gradient of Psychart.getGradientNames()) {
-    fs.writeFileSync(
-        'src/img/' + gradient.toLowerCase() + '.svg',
-        Psychart.generateGradientIcon(gradient));
+for (const name of GradientNames) {
+    const gradient: Gradient = Palette[name],
+        base: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
+        icon: SVGRectElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
+    base.append(gradient.toSVG('grad'), icon);
+    icon.setAttribute('fill', 'url(#grad)');
+    base.setAttribute('viewBox', '0 0 10 10');
+    icon.setAttribute('x', '0');
+    icon.setAttribute('y', '0');
+    icon.setAttribute('width', '10');
+    icon.setAttribute('height', '10');
+    icon.setAttribute('rx', '2');
+    fs.writeFileSync('src/img/' + name.toLowerCase() + '.svg', base.outerHTML);
 }
