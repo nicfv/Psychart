fd0ea31f58fc489419e60bb610377b92b50481fc
Nicolas Ventura <ventura@lbl.gov>
2025-04-01 15:14:59 -0700

    Add region gradient & gradient names

--- src/defaults.ts
+++ src/defaults.ts
@@ -1,19 +1,39 @@
+import { Palette, PaletteName } from 'viridis';
 import { GrafanaDataOptions, GrafanaPsychartOptions } from './types';
 
+/**
+ * Get the region gradient.
+ */
+export const regionGradient: PaletteName = 'Purplish';
+
+/**
+ * Get the axis color for this theme.
+ */
 export function getAxisColor(darkTheme: boolean): string {
     return darkTheme ? '#303030' : '#E0E0E0';
 }
 
+/**
+ * Get the font color for this theme.
+ */
 export function getFontColor(darkTheme: boolean): string {
     return darkTheme ? '#D0D0D0' : '#202020';
 }
 
+/**
+ * The list of all selectable gradient names.
+ */
+export const GradientNames: Array<PaletteName> = Object.keys(Palette).filter(name => name !== regionGradient).map(name => name as PaletteName);
+
+/**
+ * Default options for Psychart.
+ */
 export const defaultGrafanaOptions: GrafanaPsychartOptions = {
     altitude: 0,
     colors: {
         axis: '#E0E0E0',
         font: '#202020',
-        regionGradient: 'Purplish',
+        regionGradient: regionGradient,
     },
     count: 0,
     dbMax: 120,
@@ -45,6 +65,9 @@ export const defaultGrafanaOptions: GrafanaPsychartOptions = {
     yAxis: 'dp',
 };
 
+/**
+ * Default options for a data series.
+ */
 export const defaultDataOptions: GrafanaDataOptions = {
     advanced: false,
     color: '#000000',
