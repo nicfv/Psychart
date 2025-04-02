d1c725b8577fcd28f1755162eb732e3392dc4ae1
Nicolas Ventura <ventura@lbl.gov>
2025-04-02 14:16:01 -0700

    Various improvements: use `props` and `PanelDataErrorView`

--- src/components/panel.tsx
+++ src/components/panel.tsx
@@ -1,39 +1,40 @@
 import React from 'react';
 import { PanelProps } from '@grafana/data';
 import { useTheme2 } from '@grafana/ui';
-import { GrafanaPsychartOptions } from '../types';
+import { GrafanaDataOptions, GrafanaPsychartOptions } from '../types';
 import { Container } from './container';
 import { format, getFieldList } from '../formatter';
 import { Psychart } from 'psychart';
 import { getAxisColor, getFontColor } from '../defaults';
+import { PanelDataErrorView } from '@grafana/runtime';
 
-export const PsyPanel: React.FC<PanelProps<GrafanaPsychartOptions>> = ({ options, data, width, height }) => {
+export const PsyPanel: React.FC<PanelProps<GrafanaPsychartOptions>> = (props) => {
   const isDarkTheme = useTheme2().isDark,
-    errorColor = useTheme2().colors.error.text,
-    legendHeight = 0.10 * height;
+    legendHeight = 0.10 * props.height;
   try {
     const psychart: Psychart = new Psychart(
       {
-        ...options,
-        size: { x: width, y: height - legendHeight - 10 },
+        ...props.options,
+        size: { x: props.width, y: props.height - legendHeight - 10 },
         colors: {
           axis: getAxisColor(isDarkTheme),
           font: getFontColor(isDarkTheme),
           regionGradient: 'Purplish',
         },
         flipGradients: isDarkTheme,
-        flipXY: options.mollier,
-        yAxis: options.mollier ? 'hr' : 'dp',
+        flipXY: props.options.mollier,
+        yAxis: props.options.mollier ? 'hr' : 'dp',
       }
     ),
-      formatted = format(data.series),
+      formatted = format(props.data.series),
       fieldList = getFieldList(formatted),
-      startTime = data.timeRange.from.unix() * 1e3,
-      endTime = data.timeRange.to.unix() * 1e3;
+      startTime = props.data.timeRange.from.unix() * 1e3,
+      endTime = props.data.timeRange.to.unix() * 1e3;
     for (const t in formatted) {
-      for (const f in options.series || {}) {
-        if (options.series[f].seriesName && fieldList.includes(options.series[f].dryBulb) && fieldList.includes(options.series[f].other)) {
-          psychart.plot({ db: formatted[t][options.series[f].dryBulb], other: formatted[t][options.series[f].other], measurement: options.series[f].measurement }, { ...options.series[f], time: { now: +t, start: startTime, end: endTime } });
+      for (const f in props.options.series || {}) {
+        const dataOpts: GrafanaDataOptions = props.options.series[f];
+        if (dataOpts.seriesName && fieldList.includes(dataOpts.dryBulb) && fieldList.includes(dataOpts.other)) {
+          psychart.plot({ db: formatted[t][dataOpts.dryBulb], other: formatted[t][dataOpts.other], measurement: dataOpts.measurement }, { ...dataOpts, time: { now: +t, start: startTime, end: endTime } });
         }
       }
     }
@@ -48,16 +49,6 @@ export const PsyPanel: React.FC<PanelProps<GrafanaPsychartOptions>> = ({ options
       </div>
     </div>;
   } catch (ex: any) {
-    return (
-      <div style={{
-        color: errorColor,
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100%',
-      }}>
-        <h3>{ex.name + ': ' + ex.message}</h3>
-      </div>
-    );
+    return <PanelDataErrorView panelId={props.id} data={props.data} message={'' + ex} />;
   }
 };
