90d919ac5027dd294e15c54d81bb8a987a0360ed
Nicolas Ventura <ventura@lbl.gov>
2025-04-17 12:47:10 -0700

    Break out SVG ns

--- src/icon.ts
+++ src/icon.ts
@@ -1,14 +1,19 @@
 import { Gradient, Palette, PaletteName } from 'viridis';
 
 /**
+ * SVG Namespace
+ */
+const NS = 'http://www.w3.org/2000/svg';
+
+/**
  * Generate and get the URL of a gradient icon.
  */
 export function getURL(name: PaletteName, sizePx: number, cornerRadiusPx: number): string {
     const safeName: string = name.toLowerCase().replaceAll(' ', '_'),
         gradient: Gradient = Palette[name],
-        base: SVGSVGElement = document.createElementNS('http://www.w3.org/2000/svg', 'svg'),
-        icon: SVGRectElement = document.createElementNS('http://www.w3.org/2000/svg', 'rect');
-    base.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+        base: SVGSVGElement = document.createElementNS(NS, 'svg'),
+        icon: SVGRectElement = document.createElementNS(NS, 'rect');
+    base.setAttribute('xmlns', NS);
     base.setAttribute('width', sizePx.toString());
     base.setAttribute('height', sizePx.toString());
     base.setAttribute('viewBox', '0 0 ' + sizePx + ' ' + sizePx);
